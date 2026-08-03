f51a4393d2f1f95ed72e4393cdd9b07ca9abc63f
Leon Martin <alquimista3301@gmail.com>
2026-08-03 13:53:29 -0600

    Initial Commit

---
+++ XcodeClean Pro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  XcodeClean Pro
+//
+//  Created by Malva on 03/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ XcodeClean Pro/XcodeClean_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeClean_ProApp.swift
+//  XcodeClean Pro
+//
+//  Created by Malva on 03/08/26.
+//
+
+import SwiftUI
+
+@main
+struct XcodeClean_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
